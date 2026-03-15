88f061a8ff3adee464462b89ad7d7c18718e5654
matildecapelonobrega <matildecapelonobrega@gmail.com>
2026-03-15 20:49:21 +0000

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,12 +1,12 @@
 import turtle
 import time
+import random
 
-#fundo do jogo
 janela = turtle.Screen()
 janela.bgcolor("lightgreen")
 janela.setup(width=600, height=600)
+janela.tracer(0)
 
-#a cabeca da cobra
 cabeca = turtle.Turtle()
 cabeca.speed(0)
 cabeca.shape("square")
@@ -15,7 +15,6 @@ cabeca.penup()
 cabeca.goto(0,0)
 cabeca.direction = "stop"
 
-#teclado 
 def ir_para_cima():
     cabeca.direction = "up"
 
@@ -35,7 +34,6 @@ janela.onkeypress(ir_para_baixo, "s")
 janela.onkeypress(ir_para_esquerda, "a")
 janela.onkeypress(ir_para_direita, "d")
 
-#movimentos da cobra
 def mover():
     if cabeca.direction == "up":
         y = cabeca.ycor()
@@ -53,11 +51,46 @@ def mover():
         x = cabeca.xcor()
         cabeca.setx(x - 20)
 
-#loop
+comida = turtle.Turtle()
+comida.shape("circle")
+comida.color("red")
+comida.penup()
+comida.goto(0, 100)
+
+segmentos = []
+
 while True:
     janela.update()
+
+    if cabeca.xcor() > 290 or cabeca.xcor() < -290 or cabeca.ycor() > 290 or cabeca.ycor() < -290:
+        time.sleep(1)
+        cabeca.goto(0, 0)
+        cabeca.direction = "stop"
+        for segmento in segmentos:
+            segmento.goto(1000, 1000)
+        segmentos.clear() 
+
+    if cabeca.distance(comida) < 20:
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        comida.goto(x, y)
+
+        novo_segmento = turtle.Turtle()
+        novo_segmento.speed(0)
+        novo_segmento.shape("square")
+        novo_segmento.color("blue") 
+        novo_segmento.penup()
+        segmentos.append(novo_segmento)
+    
+    for index in range(len(segmentos) - 1, 0, -1):
+        x = segmentos[index-1].xcor()
+        y = segmentos[index-1].ycor()
+        segmentos[index].goto(x, y)
+
+    if len(segmentos) > 0:
+        x = cabeca.xcor()
+        y = cabeca.ycor()
+        segmentos[0].goto(x, y)
+    
     mover()
     time.sleep(0.1)
-
-turtle.done()
-
